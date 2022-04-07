1e15376c0938113ed17e2ea0f3f5f8dc54bc8811
Jeremiah Ramiscal <83548837+heyitsjearbear@users.noreply.github.com>
2022-04-06 22:51:53 -0700

    Update numberGuesser.py

--- numberGuesser.py
+++ numberGuesser.py
@@ -1,3 +1,8 @@
+"""
+Program picks a random number between 1 and 99,
+and the program keeps asking the user to guess that randomly picked
+integer. The program will close when the user correctly guesses the integer.
+"""
 import random
 # n will be the number that the computer chooses for you to guess.
 n = random.randint(1, 99)
